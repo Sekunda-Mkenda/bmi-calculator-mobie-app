e16ed47570d4c4bce99b5e81949fd097908470f6
Sekunda-Mkenda <sekundamkenda07@gmail.com>
2024-01-17 22:16:01 +0300

    adding-textfield

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,66 +14,77 @@ class _HomeScreenState extends State<HomeScreen> {
       // appBar: AppBar(title: Text('home')),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(children: [
-            Container(
-              height: 280.0,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/hero_image.png'),
-                    fit: BoxFit.contain),
-              ),
-              child: const Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.history_rounded,
-                          color: Colors.white,
-                          size: 30.0,
+          child: Column(
+            children: [
+              Container(
+                height: 280.0,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/hero_image.png'),
+                      fit: BoxFit.contain),
+                ),
+                child: const Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.history_rounded,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Text(
-                    'Your BMI is',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Text(
-                    '---',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
-                    child: Text(
-                      'Please, enter your weight and height to calculate your BMI',
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      'Your BMI is',
                       style: TextStyle(
                           color: Colors.white,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.normal,
-                          fontStyle: FontStyle.italic),
-                      textAlign: TextAlign.center,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      '---',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                      child: Text(
+                        'Please, enter your weight and height to calculate your BMI',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.normal,
+                            fontStyle: FontStyle.italic),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                  ],
+                ),
               ),
-            )
-          ]),
+
+              //Input fields
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(label: Text('Weight (Kg)')),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
